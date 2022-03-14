fe239a7af438f03598e07467a4948df73bf22b00
DejasDejas <julien.dejasmin@outlook.fr>
2022-03-14 18:38:09 -0400

    initiale commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='use xgboost to predict liver cirrhosis in a patient based on certain lifestyle and health conditions of a patients.',
+    author='Liver_cirrhosis_prediction Julien Dejasmin',
+    license='MIT',
+)
